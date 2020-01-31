e1a8abc63de24aa14ecd53f0fe302c19741e576d
joelpdurham <joeldur@gmail.com>
2020-01-30 16:41:33 -0800

    on change works!

--- src/components/form/RestForm.js
+++ src/components/form/RestForm.js
@@ -1,9 +1,9 @@
 import React from 'react';
-//import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-const RestForm = () => (
-  <form>
-    <input type="text" placeholder="URL"/>
+const RestForm = ({ url, rawJsonBody, username, password, token, onChange, onSubmit }) => (
+  <form onSubmit={onSubmit}>
+    <input type="text" name="url" value={url} onChange={onChange} placeholder="URL"/>
     <section>
       <input type="radio" name="route" value="GET" />
       <input type="radio" name="route" value="POST" />
@@ -13,17 +13,27 @@ const RestForm = () => (
     </section>
     <button>Go!</button>
 
-    <textarea />
+    <textarea value={rawJsonBody} name="rawJsonBody" onChange={onChange} placeholder="Raw JSON body" />
 
     <section>
       <button>Headers</button>
       <h3>Basic Authorization</h3>
-      <input type="text" />
-      <input type="text" />
+      <input type="text" name="username" value={username} onChange={onChange} placeholder="Username" />
+      <input type="text" name="password" value={password} onChange={onChange} placeholder="Password" />
       <h3>Bearer Token</h3>
-      <input type="text" />
+      <input type="text" name="token" value={token} onChange={onChange} placeholder="Bearer Token" />
     </section>
   </form>
 );
+
+RestForm.propTypes = {
+  url: PropTypes.string.isRequired,
+  rawJsonBody: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string .isRequired,
+  token: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired
+};
 
 export default RestForm;
--- src/containers/RestBody.js
+++ src/containers/RestBody.js
@@ -2,12 +2,30 @@ import React, { Component } from 'react';
 import RestForm from '../components/form/RestForm';
 
 export default class RestBody extends Component {
-  state = {}
+  state = {
+    url: '',
+    rawJsonBody: '',
+    username: '',
+    password: '',
+    token: ''
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  }
 
   render() {
+    const { url, rawJsonBody, username, password, token } = this.state;
     return (
       <>
-        <RestForm />
+        <RestForm 
+          url={url}
+          rawJsonBody={rawJsonBody}
+          username={username}
+          password={password}
+          token={token}
+          onChange={this.handleChange}
+        />
       </>
     );
   }
